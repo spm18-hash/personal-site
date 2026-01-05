256f7d58ebfcfdbb604eaaa03edd240e67b12a13
Smit Mistry <smitmistry19@gmail.com>
2026-01-05 21:15:26 +0530

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Google Sans Flex",
+        cssVariable: "--font-google",
+      },
+    ],
+  },
+});
